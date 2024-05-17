a40b1523b5451c491b04a72f91dc3407d5cf9ffd
Bhagyesh Nimbalkar <nimbalkarbhagyesh6979@gmail.com>
2024-05-17 22:49:41 +0530

    Some Changes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
 import { Provider } from './context/context';
 import App from './App';
 
 
-const container = document.getElementById("root");
 
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
   <Provider>
   <App/>
   </Provider>
- </StrictMode>
+ </StrictMode>,
+ document.getElementById('root')
 )
